cd01fba8f0cb2674a45635166f72ed29f751031d
wmz786 <waismalikzada9@gmail.com>
2023-12-16 15:25:19 +0430

    add two tap gesture

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,14 +1,35 @@
 import { Image, View } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
 
 const EmojiSticker = ({ imageSize, stickerSource }) => {
+  const scaleImage = useSharedValue(imageSize);
+  const doubleTap = Gesture.Tap()
+    .numberOfTaps(2)
+    .onStart(() => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    });
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
   return (
     <View style={{ top: -35 }}>
-      <Animated.Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <GestureDetector gesture={doubleTap}>
+        <Animated.Image
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </GestureDetector>
     </View>
   );
 };
